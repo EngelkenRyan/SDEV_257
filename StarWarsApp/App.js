9703d6624f76938720d7b2fa967af53d9b6bb675
RyanEngelken <75546339+RyanEngelken@users.noreply.github.com>
2025-11-07 15:59:53 -0500

    Add StarWarsApp files properly

---
+++ StarWarsApp/App.js
@@ -0,0 +1,31 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Platform } from "react-native";
+
+import Films from "../StarWarsApp/screens/Films";
+import Planets from "../StarWarsApp/screens/Planets";
+import Spaceships from "../StarWarsApp/screens/Spaceships";
+
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      {Platform.OS === "ios" ? (
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen name="Films" component={Films} />
+          <Tab.Screen name="Planets" component={Planets} />
+          <Tab.Screen name="Spaceships" component={Spaceships} />
+        </Tab.Navigator>
+      ) : (
+        <Drawer.Navigator screenOptions={{ headerShown: true }}>
+          <Drawer.Screen name="Films" component={Films} />
+          <Drawer.Screen name="Planets" component={Planets} />
+          <Drawer.Screen name="Spaceships" component={Spaceships} />
+        </Drawer.Navigator>
+      )}
+    </NavigationContainer>
+  );
+}
---
+++ StarWarsApp/screens/Films.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Films() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is the Films page</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
